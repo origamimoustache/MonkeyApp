9a2f61a62fe1b6bbd541aa25bcc66023f00f5db9
origamimoustache <origami.moustache@gmail.com>
2026-04-23 14:37:47 -0400

    Update SurviveTheArcGame.py

--- SurviveTheArcGame.py
+++ SurviveTheArcGame.py
@@ -176,12 +176,12 @@ with col1:
         HeatMap(heat_data, radius=25).add_to(m)
 
     # Current position marker
-folium.Marker(
-    location=[current["lat"], current["lon"]],
-    icon=folium.Icon(color="red")
-).add_to(m)
-
-st_folium(m, width=1100, height=700)
+    folium.Marker(
+        location=[current["lat"], current["lon"]],
+        icon=folium.Icon(color="red")
+    ).add_to(m)
+    
+    st_folium(m, width=1100, height=700)
 
 # ================== GAME PANEL ==================
 with col2:
